23d091a0d28193856fdfd5100e6b1ca31d091e4e
Siya Songo <memyselfa4@gmail.com>
2025-06-25 09:26:06 +0200

    Added Updates Models
    User, Todo

--- server/Models/TodoItem.cs
+++ server/Models/TodoItem.cs
@@ -1,13 +1,22 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace server.Models
 {
     public class TodoItem
     {
         public int Id { get; set; }
+        [Required]
         public string Text { get; set; }
+        [Required]
         public bool Completed { get; set; }
 
         public int UserId { get; set; }
+        [Required]
+        [ForeignKey("UserId")]
         public User User { get; set; }
+        
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
--- server/Models/User.cs
+++ server/Models/User.cs
@@ -1,10 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace server.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string PasswordHash { get; set; }
 
         public ICollection<TodoItem> TodoItems { get; set; }
